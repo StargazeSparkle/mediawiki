ae3603a5217be8130b35d034bdbde6e23c505fd2
StargazeSparkle <66642904+StargazeSparkle@users.noreply.github.com>
2021-01-06 21:43:28 -0500

    Added types

--- src/mediawiki/client.clj
+++ src/mediawiki/client.clj
@@ -1,39 +1,86 @@
 (ns mediawiki.client
-  (:require [mediawiki.http :refer [http-post]]))
+  (:require [mediawiki.http :as http]))
+
+;; mediawiki-session type is needed in order to manage multiple mediawiki sessions
+(deftype mediawiki-session [http-session url username password])
+
+(defn new-session
+  "Creates a new instance of mediawiki-session for multiple requests.
+  `:url` is the fully qualified path to the base of the installation.
+  `:username` is the username of the account this session will represent.
+  `:password` is the password for that session. 
+   `:cookie-jar` is an optional instance of clj-http.cookies/cookie-store."
+  [& {url        :url
+      username   :username
+      password   :password
+      cookie-jar :cookie-jar}]
+  (->mediawiki-session (http/new-session :cookie-jar cookie-jar)
+                       url
+                       username
+                       password))
+
+(defn- fetch-login-token 
+  "Attempts to fetch a login token from the API.
+   `session` is a required instance of mediawiki-session.
+   `success` is the function to call upon successful token retrieval.
+   `failure` is the function to call if there is an error fetching the token."
+  [session success failure]
+  (let [params {:action     "login"
+                :lgname     (.username session)
+                :lgpassword (.password session)
+                :format     "json"}]
+    (http/http-post (.http-session session)
+                    (.url session)
+                    "/api.php"
+                    params
+                    (fn [res] 
+                      (let [token (get-in res "login" "token")]
+                        (if (nil? token)
+                          (failure {:failed   true
+                                    :response res})
+                          (success token))))
+                    (fn [res] (failure {:failed   true
+                                        :response res})))))
+
+(defn- complete-login
+  "Attempts to complete the login with a token.
+   `session` is a required instance of mediawiki-session.
+   `token` is a required token.
+   `success` is the function to call upon a successful login.
+   `failure` is the function to call if the login action fails."
+  [session token success failure]
+  (let [params {:action     "login"
+                :lgname     (.username session)
+                :lgpassword (.password session)
+                :lgtoken    token
+                :format     "json"}]
+    (http/http-post (.http-session session)
+                    (.url session)
+                    "/api.php"
+                    params
+                    (fn [res]
+                      (let [result (get-in res "login" "result")]
+                        (if (= result "Success")
+                          (success)
+                          (failure {:failed   true
+                                    :response res}))))
+                    (fn [res] (failure {:failed   true
+                                        :response res})))))
 
 (defn login
-  "This function performs the login action on the wiki.
-   `:host` is the fully qualified path to the base wiki installation.
-   `:username` is the username for the account.
-   `:password` is the password for the account.
-   `:success` is a function that will be called on login success.
-   `:failure` is a function that will be called on login failure."
-  [& {host     :host
-      username :username
-      password :password
-      success  :success
-      failure  :failure}]
-  (let [prom  (promise)
-        pass  (fn [res] (deliver prom {:result res}))
-        fail  (fn [rej] (deliver prom {:failed true  :result rej}))
-        res   {}
-        token nil]
-    (do
-      (http-post :host      host 
-                 :end-point "/api.php"
-                 :success   pass
-                 :failure   fail
-                 :params    {:action     "login"
-                             :lgname     username
-                             :lgpassword password
-                             :format     "json"})
-      (set! res @prom)
-      (if (contains? res :failed)
-        (failure res)
-        (do
-          (set! token (get-in res ["login" "token"]))
-          (if (nil? token)
-            (failure res)
-            (http-post :host host
-                       :end-point "/api.php"
-                       :success )))))))
+  "Attempts to perform a login request to the mediawiki instance and responds through callbacks.
+   `session` is a required instance of mediawiki-session.
+   `:success` is the callback for a successful login.
+   `:failure` is the callback for a failed login."
+  [session
+   & {success :success
+      failure :failure}]
+  (fetch-login-token session
+                     (fn [token]
+                       (complete-login session
+                                       token
+                                       (fn [] (success))
+                                       (fn [res] (failure {:failed   true
+                                                           :response res}))))
+                     (fn [res] (failure {:failed   true
+                                         :response res}))))
--- src/mediawiki/http.clj
+++ src/mediawiki/http.clj
@@ -3,19 +3,27 @@
   (:require [clj-http.client   :as client]
             [clj-http.cookies  :as cookies]))
 
-;; TODO: Encapsulate this or push the responsibility to the end-user
-;; This should not be a global on the off-chance that someone wants to
-;; run multiple mediawiki instances.
-(def *http-cookie-jar* (cookies/cookie-store))
+;; http-session type is needed in order to allow for different instances of http
+(deftype http-session [cookie-jar])
+
+(defn new-session
+  "Creates a new instance of http-session for subsequent requests.
+   `:cookie-jar` is an optional instance of clj-http.cookies/cookie-store."
+  [& {cookie-jar :cookie-jar}]
+  (if (nil? cookie-jar)
+    (->http-session (cookies/cookie-store))
+    (->http-session cookie-jar)))
 
 (defn http-get
   "Perform an async HTTP GET with callbacks.
+   `session` is a required instance of http-session.
    `:host` is the fully qualified url to the base of the installation.
    `:end-point` is the end-point to use with a leading /.
    `:params` is a hash-map of paramters for the request.
    `:success` is the function to call when a request is successful.
    `:faulure` is the function to call when errors arise."
-  [& {host      :host
+  [session 
+   & {host      :host
       end-point :end-point
       params    :params
       success   :success
@@ -24,18 +32,20 @@
      (client/get url {:accept        :json
                       :async?        true
                       :cookie-policy :standard
-                      :cookie-store  *http-cookie-jar*
+                      :cookie-store  (.cookie-jar session)
                       :query-params  params}
                  success failure)))
 
 (defn http-post
   "Perform an async HTTP POST with callbacks.
+   `session` is a required instance of http-session.
    `:host` is the fully qualified url to the base of the installation.
    `:end-point` is the end-point to use with a leading /.
    `:params` is a hash-map of paramters for the request.
    `:success` is the function to call when a request is successful.
    `:faulure` is the function to call when errors arise."
-  [& {host      :host
+  [session
+   & {host      :host
       end-point :end-point
       params    :params
       success   :success
@@ -44,6 +54,6 @@
     (client/post url {:accept        :json
                       :async?        true
                       :cookie-policy :standard
-                      :cookie-store  *http-cookie-jar*
+                      :cookie-store  (.cookie-jar session)
                       :form-params   params}
                  success failure)))
